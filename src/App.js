9fbec0315feadfcf745d8d5b546c57dabf25ceac
trickster1o1 <nischaltuladhar15@gmail.com>
2024-05-20 11:54:49 +0545

    react init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import imgA from './assets/img/1.jpg';
+import imgB from './assets/img/2.jpg';
+
+function App() {
+  return (
+    <>
+      <h1>working</h1>
+    </>
+  );
+}
+
+export default App;
