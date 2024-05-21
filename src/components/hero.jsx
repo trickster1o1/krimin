7c9c362050cd3d72d1489cd8781868f3a4078aa1
trickster1o1 <nischaltuladhar15@gmail.com>
2024-05-21 13:44:57 +0545

    thumb images init

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,39 +1,45 @@
-import {gsap} from 'gsap';
-import { useEffect } from 'react';
+import { gsap } from "gsap";
+import { useEffect } from "react";
 export default function Hero() {
-
-    useEffect(() => {
-        let ctx = gsap.context(() => {
-            let tm = gsap.timeline();
-          tm.to('body', {
-            duration: 0,
-            overflow: 'hidden'
-          }).to(".inner-lense", {
-            duration: .5,
-            transform: 'scale(.8)'
-          }).to('.inner-lense', {
-            duration: .5,
-            transform: 'scale(1.5)'
-          }).to('.flash', {
-            duration: .5,
-            width: '2em',
-            height: '2em',
-          }).to('.flash', {
-            duration: .2,
-            width: '200vw',
-            height: '200vh'
-          }).to('.splash', {
-            opacity: '0',
-          }).to('body', {
-            duration: 0,
-            overflow: 'auto',
-          }).to('.splash', {
-            zIndex: '-1',
-          });
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      let tm = gsap.timeline();
+      tm.to("body", {
+        duration: 0,
+        overflow: "hidden",
+      })
+        .to(".inner-lense", {
+          duration: 0.5,
+          transform: "scale(.8)",
+        })
+        .to(".inner-lense", {
+          duration: 0.5,
+          transform: "scale(1.5)",
+        })
+        .to(".flash", {
+          duration: 0.5,
+          width: "2em",
+          height: "2em",
+        })
+        .to(".flash", {
+          duration: 0.2,
+          width: "200vw",
+          height: "200vh",
+        })
+        .to(".splash", {
+          opacity: "0",
+        })
+        .to("body", {
+          duration: 0,
+          overflow: "auto",
+        })
+        .to(".splash", {
+          zIndex: "-1",
         });
-    
-        return () => ctx.revert();
-      }, []);
+    });
+
+    return () => ctx.revert();
+  }, []);
   return (
     <>
       <div className="hero-cont" id="hero">
@@ -48,9 +54,9 @@ export default function Hero() {
         <section className="overlay"></section>
       </div>
       <div className="splash">
-        <div className='lense'>
-            <div className='inner-lense'></div>
-            <div className='flash'></div>
+        <div className="lense">
+          <div className="inner-lense"></div>
+          <div className="flash"></div>
         </div>
       </div>
     </>
